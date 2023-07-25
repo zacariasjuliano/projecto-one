70af1d8274890c444f0d1f1380e086a87237e650
zacariasjuliano <zacariasjuliano@gmail.com>
2023-07-25 19:56:49 +0100

    Melhorando a pesquisa

--- app/models/banco.rb
+++ app/models/banco.rb
@@ -1,12 +1,6 @@
 class Banco < ApplicationRecord
 
-    scope :by_nome, ->(nome) { where("lower(nome) like ?", "%#{nome.downcase}%") if nome.present?}
-    scope :by_sigla, ->(sigla) { where("lower(sigla) like ?", "%#{sigla.downcase}%") if sigla.present?}
-    scope :by_email, ->(email) { where("lower(email) like ?", "%#{email.downcase}%") if email.present?}
-    scope :by_ni, ->(ni) { where("lower(ni) like ?", "%#{ni.downcase}%") if ni.present?}
-    scope :by_capital, ->(capital) { where("lower(capital) like ?", "%#{capital.downcase}%") if capital.present?}
-    scope :by_pais, ->(pais) { where("lower(pais) like ?", "%#{pais.downcase}%") if pais.present?}
-
+    include Bancos::Busca
 
     def self.busca_por_parametro(params = {})
         bancos = Banco.all
