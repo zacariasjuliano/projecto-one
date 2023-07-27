bf6f57d613068eecde354ebb965365881acd46e5
zacariasjuliano <zacariasjuliano@gmail.com>
2023-07-27 18:52:04 +0100

    Pesquisa com Where, entre valores

--- app/services/banco_service_query.rb
+++ app/services/banco_service_query.rb
@@ -7,6 +7,10 @@ class BancoServiceQuery
         bancos = bancos.by_ni(params[:ni]) if params[:ni].present?
         bancos = bancos.by_capital(params[:capital]) if params[:capital].present?
         bancos = bancos.by_pais(params[:pais]) if params[:pais].present?
+        
+        bancos = bancos.where("capital >= ?", params[:capital_ini]) if params[:capital_ini].present?
+        bancos = bancos.where("capital <= ?", params[:capital_fim]) if params[:capital_ini].present?
+
         bancos
     end
 end
